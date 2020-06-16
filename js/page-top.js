c4765a88ef5e9fed7de602ed37be0d513bf5b5f0
Hiroaki Koizumi <hiroa3298@gmail.com>
2020-06-16 21:16:15 +0900

    【jsファイル】

---
+++ js/page-top.js
@@ -0,0 +1,16 @@
+{;}jQuery
+
+$(function() {
+  var pagetop = $('.pagetop');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 700) {
+      pagetop.fadeIn();
+    } else {
+      pagetop.fadeOut();
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+});
